69ad68f2b84e9449494d6687ce853fd11b4990c7
ali <mohammadali.aghaei@divar.ir>
2026-09-25 20:34:35 +0330

    bane name project

---
+++ Band_Name.py
@@ -0,0 +1,8 @@
+print("Hello and welcome to my Band Name Generator")
+
+city_name = input("What is the name of the city you grew up in?\n").strip()
+pet_name = input("What is your pet's name?\n").strip()
+
+band_name = f"{city_name} {pet_name}"
+
+print(f"This can be your Band Name: {band_name}")
